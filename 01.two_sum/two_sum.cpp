6cec814a0a08f7aafb918f27ee86bf0e922eca4d
rishab_7701 <agarwalrishab50@gmail.com>
2024-04-22 20:53:01 +0530

    Leetcode daily practice

--- 01.two_sum/two_sum.cpp
+++ 01.two_sum/two_sum.cpp
@@ -1,19 +1,38 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int a=0,b=0;
+
+//         for(int i=0;i<nums.size()-1;i++)
+//         {
+//             for(int j=i+1;j<nums.size();j++)
+//             {
+//                 if(nums[i]+nums[j]==target)
+//                 {
+//                     a=i;
+//                     b=j;
+//                 }
+//             }
+//         }
+//         return{a,b};
+//     }
+// };
+
+//********************** Better and Optimized Solution using Hashmap ************
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int a=0,b=0;
+       map <int,int> mp;
 
-        for(int i=0;i<nums.size()-1;i++)
-        {
-            for(int j=i+1;j<nums.size();j++)
-            {
-                if(nums[i]+nums[j]==target)
-                {
-                    a=i;
-                    b=j;
-                }
+       for(int i=0;i<nums.size();i++){
+            int a=nums[i];
+            int moreNeeded = target-a;
+            if(mp.find(moreNeeded)!=mp.end()){
+                return {mp[moreNeeded],i};
             }
+         mp[a]=i;
         }
-        return{a,b};
+        return {-1,-1};
     }
 };
