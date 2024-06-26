40d00cc49f6b561841b4214e7433eaf11a1ff030
rishab_7701 <agarwalrishab50@gmail.com>
2024-06-26 18:42:49 +0530

    Leetcode daily practice

---
+++ 20.Valid Parentheses/valid_parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        int n=s.size();
+
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+                st.push(s[i]);
+            }
+            else {
+                if(st.empty()) return false;
+                else{
+                    if((s[i]==')' && st.top()=='(') || (s[i]==']' && st.top()=='[') || (s[i]=='}' && st.top()=='{')){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            } 
+        }
+        if(st.empty()) return true;
+        else return false;
+    }
+};
